9a2814d61406dd58d032d5c677c528d2a56d87c3
adhuymarwadan <akuntesting610@gmail.com>
2024-11-26 16:34:25 +0700

    commit by yudha

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}> Tugas Praktikum Pertemuan 2</Text>
+      <Text style={styles.text}> Yudha Dwi Darmawan</Text>
+      <View style={styles.boxContainer}>
+        <View style={styles.box1} />
+        <View style={styles.box2} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#D3D3D3",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  text: {
+    fontSize: 24,
+    color: "grey",
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  boxContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+  box1: {
+    width: 100,
+    height: 100,
+    backgroundColor: "black",
+    borderRadius: 10,
+    shadowColor: "purple",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+  box2: {
+    width: 100,
+    height: 100,
+    backgroundColor: "white",
+    borderRadius: 10,
+    shadowColor: "blue",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 5,
+    elevation: 5,
   },
 });
